4eca93dd6f0332ed3d6a31bb1605f74c7d7d9547
miadong <dong.xinm@husky.neu.edu>
2019-11-09 01:57:47 -0500

    change config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :time2, Time2Web.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [host: "time2.elephantdong.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
